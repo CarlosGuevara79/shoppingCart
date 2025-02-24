1dbab7a5e34ffbf9cd2c9cb21b3d2b94ed670b69
Carlos Guevara <43367164+CarlosGuevara79@users.noreply.github.com>
2025-02-23 19:06:35 -0600

    Update header.js

--- components/header.js
+++ components/header.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const header = document.createElement("header");
     header.innerHTML = `
         <div class="banner">
-            <img src="/assets/img/banner.png" class="img_banner" id="Banner_img">
+            <img src="../assets/img/banner.png" class="img_banner" id="Banner_img">
         </div>
     `;
     document.body.prepend(header); // Agrega el header al inicio del <body>
