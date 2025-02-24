18b31139c5bd22c15596985c3ba894822c26f7d3
Carlos Guevara <43367164+CarlosGuevara79@users.noreply.github.com>
2025-02-23 19:20:53 -0600

    Update header.js

--- components/header.js
+++ components/header.js
@@ -1,8 +1,12 @@
+const basePath = window.location.pathname.includes("shoppingCart") 
+    ? "/shoppingCart" 
+    : "";
+
 document.addEventListener("DOMContentLoaded", function () {
     const header = document.createElement("header");
     header.innerHTML = `
         <div class="banner">
-            <img src="./assets/img/banner.png" class="img_banner" id="Banner_img">
+            <img src="${basePath}/assets/img/banner.png" class="img_banner" id="Banner_img">
         </div>
     `;
     document.body.prepend(header); // Agrega el header al inicio del <body>
